aec2f9aa8d2472336adc70738b6923f1fc0aafce
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-17 00:07:13 -0600

    "feat: Actualización de rutas para women y producto"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar';
+import Women from './pages/women/women';
+import Producto from './pages/producto/producto';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleAddToCart = (nombre, cantidad, precio) => {
+    console.log('Agregar al carrito:', nombre, cantidad, precio);
+  };
 
   return (
     <>
       <Navbar />
-      <h1> Mi ecommerce</h1>
+      <Routes>
+        <Route path="/women" element={<Women onAddToCart={handleAddToCart} />} />
+        <Route path="/producto/:id" element={<Producto onAddToCart={handleAddToCart} />} />
+      </Routes>
     </>
   );
 }
 
 export default App;
+
