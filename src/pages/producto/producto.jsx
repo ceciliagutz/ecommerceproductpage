70203673df79e5eadebebb60669bd72dccfc921e
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-17 01:45:40 -0600

    "feat: agregue lightbox con navegación"

--- src/pages/producto/producto.jsx
+++ src/pages/producto/producto.jsx
@@ -11,6 +11,8 @@ const Producto = ({ onAddToCart }) => {
   const [cantidad, setCantidad] = useState(1);
   const [imagenActual, setImagenActual] = useState(producto?.imagenes?.principal);
 
+const [lightboxAbierto, setLightboxAbierto] =useState(false);
+const[indiceImagen, setIndiceImagen] = useState(0);
 
   if (!producto) return <h2>Producto no encontrado</h2>;
 
@@ -19,10 +21,22 @@ const Producto = ({ onAddToCart }) => {
   const incrementar = () => setCantidad(cantidad + 1);
   const decrementar = () => cantidad > 1 && setCantidad(cantidad - 1);
 
+  const abrirLightbox = (index) => {
+    setIndiceImagen(index);
+    setLightboxAbierto(true);
+  };
+  const siguienteImagen =() =>{
+    setIndiceImagen((indiceImagen +1 ) % imagenes.length);
+  };
+  const anteriorImagen =() =>{
+    setIndiceImagen ((indiceImagen -1 + imagenes.length) % imagenes.length);
+  };
+const imagenes = [producto.imagenes.principal, ...producto.imagenes.miniaturas];
+
   return (
     <div className="producto-detalle">
       <div className="imagenes-producto">
-        <img src={imagenActual} alt={producto.nombre} className="imagen-producto"/>
+        <img src={imagenActual} alt={producto.nombre} className="imagen-producto" onClick={() => abrirLightbox(imagenes.indexOf(imagenActual))}/>
         
         
         <div className="miniaturas">
@@ -66,6 +80,29 @@ const Producto = ({ onAddToCart }) => {
           <FaShoppingCart/> Agregar al carrito
         </button>
       </div>
+      
+      {lightboxAbierto && (
+        <div className="lightbox-overlay">
+          <button className="cerrar" onClick={() => setLightboxAbierto(false)}>
+            <FaTimes />
+          </button>
+
+          <button className="flecha izquierda" onClick={anteriorImagen}>
+            <FaChevronLeft />
+          </button>
+
+          <img 
+            src={imagenes[indiceImagen]} 
+            alt="Imagen ampliada" 
+            className="imagen-lightbox" 
+          />
+
+          <button className="flecha derecha" onClick={siguienteImagen}>
+            <FaChevronRight />
+          </button>
+        </div>
+
+      )}
     </div>
   );
 };
