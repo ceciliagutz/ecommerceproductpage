020f77dc7a6b5fb298b4e509c03bac5d017127d9
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-16 20:12:13 -0600

    feat: limitar ancho en desktop, reordenar iconos, ajustar espacis, responsivo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,51 +5,45 @@ import "./navbar.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const toggleMenu = () => { setMenuOpen(!menuOpen); };
 
   return (
     <nav className="navbar">
-     
-      <div className="logo">
-        <Link to="/">sneakers</Link>
-      </div>
-
-    
-      <ul className="nav-links">
-        <li><Link to="/collections">Collections</Link></li>
-        <li><Link to="/men">Men</Link></li>
-        <li><Link to="/women">Women</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-
-      
-      <div className="desktop-icons">
-        <Link to="/cart"><FaShoppingCart size={22} /></Link>
-        <Link to="/profile"><FaUserCircle size={22} /></Link>
-      </div>
-
-      
-      <button className="menu-toggle" onClick={toggleMenu} aria-label="Menú">
+      <div className="navbar-container">
+ <button className="menu-toggle" onClick={toggleMenu} aria-label="Menú">
         {menuOpen ? <FaTimes size={25} /> : <FaBars size={25} />}
       </button>
 
-  
-      <aside className={`side-menu ${menuOpen ? "active" : ""}`}>
-        <ul>
+        <div className="logo">
+          <Link to="/">sneakers</Link>
+        </div>
+<div className="nav-left">
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`} >
+          <button className="close-menu" onClick={toggleMenu} aria-label="Cerrar menú">
+            <FaTimes size={25} />
+          </button>
           <li><Link to="/collections" onClick={toggleMenu}>Collections</Link></li>
           <li><Link to="/men" onClick={toggleMenu}>Men</Link></li>
           <li><Link to="/women" onClick={toggleMenu}>Women</Link></li>
           <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
           <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
         </ul>
-        <div className="mobile-icons">
-          <Link to="/cart"><FaShoppingCart size={22} /></Link>
-          <Link to="/profile"><FaUserCircle size={22} /></Link>
-        </div>
-      </aside>
+      </div>
+      <div className="nav-icons">
+        <Link to="/cart" aria-label="Carrito de compras">
+          <FaShoppingCart size={22} color="#69707d"/>
+        </Link>
+        <Link to="/profile" aria-label="Perfil">
+          <FaUserCircle size={22} color="#69707d"/>
+        </Link>
+      </div>
+      </div>
+     
+      {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
     </nav>
   );
 }
 
 export default Navbar;
+
