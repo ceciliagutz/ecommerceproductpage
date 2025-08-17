a34353610bb39836de8967a34cec922a48a652f1
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-16 23:34:25 -0600

    "feat: Creación y actualización de la página Women"

---
+++ src/pages/women/women.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import TarjetaProducto from '../../components/tarjeta_productos/tarjetaProductos';
+import products from '../../data/products.json';
+import './women.css';
+
+const Women = () => {
+  const handleAddToCart = (nombre, cantidad, precio) => {
+    console.log('Agregar al carrito:', nombre, cantidad, precio);
+    alert(`Producto agregado: ${nombre}\nCantidad: ${cantidad}\nPrecio: $${precio}.00`);
+  };
+
+  return (
+    <div className="container-women">
+      {products.map((prod) => (
+        <TarjetaProducto
+          key={prod.id}
+          nombre={prod.nombre}
+          marca={prod.marca}
+          precio={prod.precio}
+          precioOriginal={prod.precioOriginal}
+          descripcion={prod.descripcion}
+          imagenPrincipal={prod.imagenes.principal}
+          onAddToCart={handleAddToCart}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Women;
+
+
+
+
